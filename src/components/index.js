973dc4f14547f6c88b8e67602873cb62545476a0
koozoo <meteoramtr@gmail.com>
2022-08-16 13:11:32 +0400

    sort done

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Categories from './Categories';
 import CatalogItem from './CatalogItem';
 import Catalog from './Catalog';
 import Button from './Button';
+import SortPopup from './SortPopup';
 
-export { Header, Footer, Categories, CatalogItem, Catalog, Button };
+export { Header, Footer, Categories, CatalogItem, Catalog, Button, SortPopup };
